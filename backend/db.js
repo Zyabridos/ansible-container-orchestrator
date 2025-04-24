28204be3d4515c55f96fe5457ee17c6525346566
Nina Zyabrina <zyabrina95@gmail.com>
2025-04-24 17:35:52 +0200

    updt ssl settings

--- backend/db.js
+++ backend/db.js
@@ -3,13 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const ssl =
+  process.env.DB_SSL === 'true'
+    ? { rejectUnauthorized: false }
+    : false;
+
 const pool = new pg.Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  ssl: process.env.DB_SSL === 'true',
+  ssl,
 });
 
 export default pool;
