abfefc10a8838ac76d3027452386c7993965df4e
Nina Zyabrina <zyabrina95@gmail.com>
2025-04-24 17:32:45 +0200

    add neccesary packages

--- backend/testDbConnection.js
+++ backend/testDbConnection.js
@@ -1,12 +1,14 @@
 import pool from './db.js';
+import { existsSync } from 'fs';
 
 (async () => {
+  console.log('.env exists:', existsSync('.env'));
   try {
     const res = await pool.query('SELECT NOW()');
-    console.log('✅ DB Connected:', res.rows[0]);
+    console.log('DB Connected:', res.rows[0]);
     process.exit(0);
   } catch (err) {
-    console.error('❌ DB Connection failed:', err.message);
+    console.error('DB Connection failed:', err.message);
     process.exit(1);
   }
 })();
